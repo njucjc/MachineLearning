982fcbc6ec99829144497100f991462cd18e62ce
njucjc <njucjc@gmail.com>
2018-12-03 14:58:13 +0800

    modify fomat

--- Assignment1/isomap.py
+++ Assignment1/isomap.py
@@ -18,46 +18,48 @@ def get_dis_matrix(data):
     return res
 
 def floyd(m,k=10):
-
-	inf = np.max(m)*1000
-	row, col=m.shape
-	d1 = np.ones((row, row)) * inf
-
-	m_idx = np.argsort(m, axis=1)
-	for i in range(row):
-		d1[i, m_idx[i,0:k+1]] = m[i, m_idx[i,0:k+1]]
-	for k in range(row):
-		for i in range(row):
-			for j in range(row):
-				if d1[i, k] + d1[k, j] < d1[i, j]:
-					d1[i, j] = d1[i, k]+d1[k, j]
-	return d1
+    inf = np.max(m)*1000
+    row, col=m.shape
+    d1 = np.ones((row, row)) * inf
+    
+    m_idx = np.argsort(m, axis=1)
+    
+    for i in range(row):
+        d1[i, m_idx[i,0:k+1]] = m[i, m_idx[i,0:k+1]]
+    
+    for k in range(row):
+        for i in range(row):
+            for j in range(row):
+                if d1[i, k] + d1[k, j] < d1[i, j]:
+                    d1[i, j] = d1[i, k]+d1[k, j]
+    return d1
 
 def mds(m):
-	row, col = m.shape
-	d2 = np.square(m)
-
-	di = np.sum(d2, axis=1) / row
-	dj = np.sum(d2, axis=0) / row
-	dij = np.sum(d2) / (row ** 2)
-	b = np.zeros((row, row))
-	for i in range(row):
-		for j in range(col):
-			b[i, j] = (dij + d2[i, j] - di[i] - dj[j]) / (-2)
-	return b
+    row, col = m.shape
+    d2 = np.square(m)
+    
+    di = np.sum(d2, axis=1) / row
+    dj = np.sum(d2, axis=0) / row
+    
+    dij = np.sum(d2) / (row ** 2)
+    b = np.zeros((row, row))
+    for i in range(row):
+        for j in range(col):
+            b[i, j] = (dij + d2[i, j] - di[i] - dj[j]) / (-2)
+    return b
 
 def isomap(data, n=10, k=10):
-    # """
-    # 输入：原始矩阵，降维后的维度，k近邻
-    # 输出：降维后的矩阵
-    # """
-	m = get_dis_matrix(data)
-	m = floyd(m,k)
-	b = mds(m)
-	eig_vals, eig_vecs = np.linalg.eigh(b)
-	eig_vals_idx = np.argsort(-eig_vals)
-	eig_vals = eig_vals[eig_vals_idx]
-	eig_vecs = eig_vecs[:,eig_vals_idx]
-	eig_vals_z = np.diag(eig_vals[0:n])
-	eig_vecs_z = eig_vecs[:,0:n]
-	return np.dot(eig_vecs_z, np.sqrt(eig_vals_z))
+    """
+    输入：原始数据矩阵、降维后的维度、k近邻
+    输出：降维后的矩阵
+    """
+    m = get_dis_matrix(data)
+    m = floyd(m,k)
+    b = mds(m)
+    eig_vals, eig_vecs = np.linalg.eigh(b)
+    eig_vals_idx = np.argsort(-eig_vals)
+    eig_vals = eig_vals[eig_vals_idx]
+    eig_vecs = eig_vecs[:,eig_vals_idx]
+    eig_vals_z = np.diag(eig_vals[0:n])
+    eig_vecs_z = eig_vecs[:,0:n]
+    return np.dot(eig_vecs_z, np.sqrt(eig_vals_z))
