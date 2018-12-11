7799e7c172da889fae80520cf941adf96a59f442
njucjc <njucjc@gmail.com>
2018-12-11 19:06:35 +0800

    add data

---
+++ Assignment2/LDA/data_preprocess.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author: Jinci Chen
+import re
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize
+from utils import load_data, save_data
+
+
+def filter_stopwods(stop_words, words):
+    """
+    Input: a list of stop words, a list of words
+    Output: a list of words that filtered stop words
+    """
+    filtered_words = [w.lower() for w in words if not w.lower() in stop_words]
+    return filtered_words
+
+def filter_punc(words):
+    """
+    Input: a list of words
+    Output: a list of words the filter punctuation
+    """
+    filtered_words = [w for w in words if re.search('[A-Za-z]', w)]
+    return filtered_words
+
+if __name__ == '__main__':
+    content = load_data('data/news.txt')
+    stop_words = set(stopwords.words('english'))
+
+    corpus = []
+    for line in content:
+        words = word_tokenize(line)
+        words = filter_stopwods(stop_words, words)
+        words = filter_punc(words)
+        corpus.append(' '.join(words))
+    save_data(corpus, 'data/corpus.txt')
+
